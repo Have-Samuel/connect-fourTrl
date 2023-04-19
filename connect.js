5e967522d0a0c81f2caa4c7058cfaeb1940b3f07
Have Samuel <samiehave9@gmail.com>
2023-04-19 20:21:42 +0300

    Display the tile to the board

--- connect.js
+++ connect.js
@@ -7,11 +7,14 @@ const currPlayer = playerRed; // active player: 1 or 2
 const gameOver = false;
 let board; // array of rows, each row is array of cells  (board[y][x])
 
-const rows = 7;
-const cols = 6;
+const rows = 6;
+const cols = 7;
 
 // Set up the tiles within the board
 function setGame() {
+  // create a new board
+  board = [];
+
   for (let r = 0; r < rows; r += 1) {
     // create a new row
     const row = [];
@@ -59,36 +62,36 @@ function makeHtmlBoard() {
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
-function checkForWin() {
-  function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
+// function checkForWin() {
+//   function _win(cells) {
+//     // Check four cells to see if they're all color of current player
+//     //  - cells: list of four (y, x) cells
+//     //  - returns true if all are legal coordinates & all match currPlayer
 
-    return cells.every(
-      ([y, x]) => y >= 0
-        && y < HEIGHT
-        && x >= 0
-        && x < WIDTH
-        && board[y][x] === currPlayer,
-    );
-  }
+//     return cells.every(
+//       ([y, x]) => y >= 0
+//         && y < HEIGHT
+//         && x >= 0
+//         && x < WIDTH
+//         && board[y][x] === currPlayer,
+//     );
+//   }
 
-  // TODO: read and understand this code. Add comments to help you.
+//   // TODO: read and understand this code. Add comments to help you.
 
-  for (let y = 0; y < HEIGHT; y += 1) {
-    for (let x = 0; x < WIDTH; x += 1) {
-      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+//   for (let y = 0; y < HEIGHT; y += 1) {
+//     for (let x = 0; x < WIDTH; x += 1) {
+//       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+//       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+//       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+//       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
-      }
-    }
-  }
-}
+//       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+//         return true;
+//       }
+//     }
+//   }
+// }
 
 // makeBoard();
 makeHtmlBoard();
